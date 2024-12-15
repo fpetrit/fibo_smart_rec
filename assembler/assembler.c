7a5c72df9df3f2642f16f6e0ccd0a831ef495905
Frédéric-Henri Petrit <frederic-henri.petrit@dauphine.eu>
2024-12-15 23:16:56 +0100

    finis la fonction de parsing

--- assembler/assembler.c
+++ assembler/assembler.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <math.h>
 #include "preprocessor_consts.h"
 #include "Label_vector.h"
 #include "Error_vector.h"
+#include "opstring_mapping.h"
 
 /**
  * opstring are stored in signed char (1 byte)
@@ -34,14 +36,6 @@
  */
 static void decimal_to_hex_string(signed short n, char s[]){
 
-    // sprintf "x" conversion specifier converts signed integers types to two's complement representation hexadecimal
-    // we do not want that
-    // cast a signed type to an unsigned type do not keep the correct absolute value
-    // we must work with abs() from stdlib to have signed positive numbers
-    // abs() supports int argument
-
-    // "%0.4x" format string : flag field: '0' for left padding 0s, precision field: .4 for a 4 digits result
-
     char sign[2] = "";
 
     if (n < 0)
@@ -50,12 +44,12 @@ static void decimal_to_hex_string(signed short n, char s[]){
     sprintf(s, "%s%0.4x", sign, abs( (int)n) );
 }
 
-// for opcodes
-static void decimal_to_hex_string(unsigned char n, char s[]){
-
-    // sprintf "x" conversion specifier works as we want for unsigned types : no two's complement representation.
-
+static void decimal_to_hex_string(unsigned char n, char * s){
     sprintf(s, "0.4x", n);
+}
+
+static inline void short_to_str(signed short number, char * s){
+    sprintf(s, "%d", number);
 }
 
 
@@ -68,27 +62,27 @@ static void decimal_to_hex_string(unsigned char n, char s[]){
  * if opstring is jmp, jnz, call, is the operand a label and does the label exists
  * we suppose that we cannot reference a label that has not been defined in the previous lines
  */
-static unsigned char get_from_line(Error_vector ** errors, char * src_line, char * label, char * opstring, signed short * operand);
+static unsigned char get_from_line(Error_vector ** errors, char ** src_line, char ** label, char ** opstring, signed short * operand);
 
 static void check_labels(Label_vector * labels, Error_vector ** errors);
 
 static void check_opstring_operand(Error_vector ** errors, char * opstring, unsigned short operand);
 
-// print all the line-level errors
-// check and print file-level errors line (e.g. labels, constant limits)
-// constructs a Label_arr structure and check its coherence
-// for the moment the caller is responsible for the lables array to be large enough
-// replaces the file position indicator at begining of the file
 unsigned char parse(FILE * src, Label_vector ** labels){
 
     char line[SRC_LINE_MAX_LEN + 1];
     unsigned int line_no = 0;
     int line_len = 0;
-    char label[LABEL_MAX_CHAR_NO + 1];
+
+    char label[LABEL_MAX_LEN + 1];
     char opstring[OPSTRING_MAX_LENGTH + 1];
-    signed short operand;
-    unsigned int address = 0;
     char opcode;
+
+    signed short operand_short;
+    char operand_str[6];
+
+    unsigned int address = 0;
+    int tmp;
 
     line[0] = '\0';
 
@@ -104,21 +98,57 @@ unsigned char parse(FILE * src, Label_vector ** labels){
 
         fgets(line, SRC_LINE_MAX_LEN + 1, src);
 
-        get_from_line(&errors, line, label, opstring, &operand);
+        // GETTING DATA AND CHECKING SYNTAX ERRORS
 
-        if (opstring)
+        get_from_line(&errors, &line, &label, &opstring, &operand_short);
+
+        // if opstring is NULL, blank line or syntax error, we ignore
+        if (opstring){
+        
             address++;
 
-        check_opstring_operand(&errors, opstring, operand);
+            // OPSTRING OPERAND COHERENCE CHECKING
 
-        if ( label ){
-            Label_vector_create_label(labels, label, address);
+            check_opstring_operand(&errors, opstring, operand_short);
+
+            // LABELS HANDLING
+
+            // there is a label declaration
+            if ( label ){
+                tmp = Label_vector_search(*labels, label);
+
+                // label already referenced: must set the adress
+                if (tmp != -1)
+                    (*labels)->arr[tmp]->address = address;
+                // label has never been referenced: we create the label and set the adress
+                else
+                    Label_vector_create_label(labels, label, address);
+            }
+            
+            opcode = opstring_to_opcode(opstring, line_len);
+
+            // a label is being referenced
+            if (opcode == 5 || opcode == 6 || opcode == 7){
+
+                // operand_str now contains the label name
+                short_to_str(operand_short, operand_str);
+
+                tmp = Label_vector_search(*labels, operand_str);
+
+                // label has never been declared
+                if ( tmp == -1)
+                    Label_vector_create_label(&labels, operand_str, -1);
+                    // if there is no further declaration, adress will remained -1 and it will be detected during check_labels
+
+                // tmp != 1: label has been declared, do nothing
+            } 
         }
 
-        
-    }
+    } // end of while loop
 
     check_labels(* labels, &errors);
+
+    fseek(src, 0, SEEK_SET);
     
     Error_vector_display(errors);
     
