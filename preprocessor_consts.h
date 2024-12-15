1e5a273bed68737923d4870f5ed42acb2ddee47f
Frédéric-Henri Petrit <frederic-henri.petrit@dauphine.eu>
2024-12-15 22:06:46 +0100

    correction des constantes

--- preprocessor_consts.h
+++ preprocessor_consts.h
@@ -1,12 +1,10 @@
 #ifndef PREPROCESSOR_CONSTS_H
 #define PREPROCESSOR_CONSTS_H
 
-#define SRC_LINE_MAX_LEN            30
-#define SRC_INSTRUCTIONS_MAX_NO     500
-#define SRC_LINE_CHAR_MAX_NO        100
-#define LABEL_MAX_CHAR_NO           30
+#define SRC_LINE_MAX_LEN            100
+#define INSTRUCTIONS_MAX_NO         500
+#define LABEL_MAX_LEN               30
 #define LABEL_MAX_NO                60
-#define OPSTRING_MAX_LENGTH         5     
+#define OPSTRING_MAX_LENGTH         5
 
 #endif
-
