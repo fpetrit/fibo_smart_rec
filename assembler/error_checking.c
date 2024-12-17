b944e6d99f2592a31fd438e5782599157292e472
Frédéric-Henri Petrit <frederic-henri.petrit@dauphine.eu>
2024-12-18 00:45:17 +0100

    avancer error_checking

--- assembler/error_checking.c
+++ assembler/error_checking.c
@@ -5,59 +5,50 @@
 #include "Label_vector.h"
 
 
+static void skip_whitespaces_tab(char ** s, int * len){
+    while (  s[0] != '\0' & (s[0] == '\t' || s[0] == ' ' ) ){
+        s++;
+        *len--;
+    }
+}
+
+static void remove_final_new_line(char * s, int * len){
+    if (len != 0 & s[*len - 1] == '\n'){
+            s[*len - 1] = '\0';
+            *len--;
+        }
+}
+
+
 bool get_from_line(Error_vector ** errors, char * src_line, int line_no, int line_len, char * label, char * opstring, unsigned char * opcode, signed short * operand){
 
     bool skip = true;
     
-    char tmp[SRC_LINE_MAX_LEN];
-    strcpy(tmp, src_line);
-    int tmp_len = line_len;
-    char * tmp_p = tmp;
+    char line_tmp[SRC_LINE_MAX_LEN];
+    strcpy(line_tmp, src_line);
+    char * line_tmp_p = line_tmp;
 
-    // error or empty values
+    char word_tmp[LABEL_MAX_LEN];
+    int word_tmp_len = 0;
+
+    int line_tmp_len = line_len;
+    int offset;
+
+    // initialize to error/empty values
     opstring[0] = '\0';
     * opcode = 255;
     label[0] = '\0';
 
-    // %n conversion specifier also counts the leading whitespaces and excluded characters in %[ set conv. spec.
-    // the white spaces usual skipping is suppressed
+    remove_final_new_line(line_tmp_p, &line_tmp_len); 
 
-    //remove \n, guaranteed not at another place than tmp_len - 1
-    if (tmp_len != 0 & tmp_p[tmp_len - 1] == '\n'){
-        tmp_p[tmp_len - 1] = '\0';
-        tmp_len--;
-    }
+    skip_whitespaces_tab(&line_tmp_p, &line_tmp_len);    
 
-    // skip white spaces and \t
-    while (  tmp_p[0] != '\0' & (tmp_p[0] == '\t' || tmp_p[0] == ' ' ) ){
-        tmp_p++;
-        // only mmodified locally
-        tmp_len--;
-    }
+    // get the opstring or the label
 
-    // opstring ? label ?
-    if (tmp_len != 0){
+    sscanf(word_tmp, "%[^:\t ]%n", line_tmp_p, &offset);
+    line_tmp_p += offset;
+    line_tmp_len -= offset;
 
-        if (tmp_p[tmp_len - 1] == ':'){
-
-            if ( tmp_len == 1 )
-                Error_vector_create_error(errors, 9, line_no, src_line, line_len);
-            else
-                strcpy(label, tmp_p);
-
-        }
-        
-        else {
-            
-            strcpy(opstring, tmp_p);
-            * opcode = opstring_to_opcode(opstring, (size_t)tmp_len);
-            if ( * opcode == 255){
-                opstring[0] = '\0';
-                * opcode = NULL;
-                Error_vector_create_error(errors, 2, line_no, src_line, line_len);
-            }    
-        }
-    }
 
     // if we have a label we must search for the opstring, set the opstring and opcode
    
