2674a4719b44f19f8a1479318ca912943ad0b123
Frédéric-Henri Petrit <frederic-henri.petrit@dauphine.eu>
2024-12-19 03:38:34 +0100

    _

--- assembler/error_checking.h
+++ assembler/error_checking.h
@@ -1,13 +1,13 @@
 #ifndef ERROR_CHECKING_H
 #define ERROR_CHECKING_H
 
-#include "Error_vector.h"
+#include "Error.h"
 #include "Label_vector.h"
 
-bool get_from_line(Error_vector ** errors, char * src_line, int line_no, int line_len, char * label, char * opstring, unsigned char * opcode, signed short * operand);
+bool check_line(Cheking_infos * infos, char * label, char * opstring, signed short * operand);
 
-void check_labels(Label_vector * labels, Error_vector ** errors);
+void check_labels(Label_vector * labels, Error_array error_arr);
 
-void check_opstring_operand(Error_vector ** errors, char * opstring, unsigned short operand);
+bool check_opcode_operand(Cheking_infos * infos, unsigned char opcode, signed short operand);
 
 #endif
