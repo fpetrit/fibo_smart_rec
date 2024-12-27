56b4b768f116cabd8b9a8401d13a3772368981d3
Frédéric-Henri Petrit <frederic-henri.petrit@dauphine.eu>
2024-12-28 00:11:36 +0100

    fix petit problème accès au tableau de label dans label_vector_search

--- assembler/Label_vector.c
+++ assembler/Label_vector.c
@@ -41,7 +41,10 @@ int Label_vector_search(Label_vector * vect, char * name){
     int i = 0;
     bool found = 0;
     while (! found && i < vect->count){
-        found = (! strcmp(vect->arr[i]->name, name) );
+        // (*vect->arr) is a Label * (adress of the first Label in the array)
+        // parenthesis are necessary because of [] operator higher precedence
+        // we use [] on a Label * to add i bytes to the first adress and dereference it: the result is a Label (must use . operator)
+        found = (! strcmp( (*vect->arr)[i].name, name) );
         i++;
     }
 
