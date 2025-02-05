3a0f70a2e799407ff87d6f12d9d1459533bef7d8
Frédéric-Henri Petrit <frederic-henri.petrit@dauphine.eu>
2025-02-05 23:20:34 +0100

    modifié le vecteur de labels : tableau de labels plutot qu'un tableau de pointeur sur structures labels allouées dynamiquement, beaucoup moins de malloc

--- assembler/Label_vector.c
+++ assembler/Label_vector.c
@@ -5,34 +5,38 @@
 
 #define VECTOR_INITIAL_LENGTH 25
 
-// Calcul la nouvelle taille du label vector lors d'un aggrandissement
+/// @brief Compute a new larger length from the previous one when the @ref Label_vector::arr is full. 
 static inline int new_length(int length){   
     return 2*length + 1;
 }
 
-
 Label_vector * Label_vector_construct(void){
     Label_vector * res = malloc(sizeof(Label_vector));
-    res->length = VECTOR_INITIAL_LENGTH;
-    res->count = 0;
-    res->arr = malloc(sizeof(Label *) * res->length);
+
+    if (res) {
+        res->arr = malloc(sizeof(Label) * res->length);
+        res->length = res->arr ? VECTOR_INITIAL_LENGTH : 0;
+        res->count = 0;
+    }
+    
     return res;
 }
 
 Label * Label_vector_create_label(Label_vector * vect, char * name, int address, int line_no){
 
-    Label * label = malloc(sizeof(Label));
-    strcpy(label->name, name);
-    label->address = address;
-    label->line_no = line_no;
-
     if (vect->count == vect->length){
-        vect->length = new_length(vect->length);
-        vect->arr = realloc(vect->arr, sizeof(Label *) * vect->length);
+        vect->arr = realloc(vect->arr, sizeof(Label) * new_length(vect->length));
+        vect->length = vect->arr ? new_length(vect->length) : 0;
     }
 
-    vect->arr[vect->count] = label;
-    vect->count++;
+    Label * label = vect->arr ? &vect->arr[vect->count] : NULL;
+
+    if (label) {
+        strcpy(label->name, name);
+        label->address = address;
+        label->line_no = line_no;
+        vect->count++;
+    }
 
     return label;
 }
@@ -42,21 +46,13 @@ int Label_vector_search(Label_vector * vect, char * name){
     int i = 0;
     bool found = 0;
     while (! found && i < vect->count){
-        // arr is an array of Label * (aka Label **), can use [] on it
-        found = (! strcmp( vect->arr[i]->name, name) );
+        found = (! strcmp( vect->arr[i].name, name) );
         i++;
     }
 
     return found ? i - 1 : -1;
 }
 
-void Label_vector_deconstruct(Label_vector * vect){
-
-    int i = 0;
-    while (i < vect->count){
-        free(vect->arr[i]);
-        i++;
-    }
-
+inline void Label_vector_deconstruct(Label_vector * vect){
     free(vect);
 }
